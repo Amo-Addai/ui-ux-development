2c4e7fe8811accc4b21bd9c4d1e7ea49ef4785d0
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2024-01-12 16:56:49 +0000

    .

---
+++ 20/script.js
@@ -0,0 +1,18 @@
+const buttons = document.querySelectorAll('.ripple')
+
+buttons.forEach(b => b.addEventListener('click', function (e) {
+    const x = e.clientX; const y = e.clientY
+    // console.log(x, y)
+    const buttonTop = e.target.offsetTop
+    const buttonLeft = e.target.offsetLeft
+    // console.log(buttonTop, buttonLeft)
+    const xInside = x - buttonLeft
+    const yInside = y - buttonTop
+    // console.log(xInside, yInside)
+    const circle = document.createElement('span')
+    circle.classList.add('circle')
+    circle.style.top = yInside + 'px'
+    circle.style.left = xInside + 'px'
+    this.appendChild(circle) // this keyword only works with a regular function(), not an arrow => function
+    setTimeout(_ => circle.remove(), 500)
+}))
