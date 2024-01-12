6d8f69f750a328aba4ce4afa2066a442df0a07e9
Amo-Addai <kwadwoamoad@gmail.com>
2024-01-12 16:49:21 +0000

    .

--- 19/script.js
+++ 19/script.js
@@ -19,21 +19,28 @@ toggle.addEventListener('click', e => {
     }
 })
 
-function setTime() {
-    const time = new Date()
-    // console.log(time)
-    const month = time.getMonth(),
-    day = time.getDay(),
-    hours = time.getHours(),
-    minutes = time.getMinutes(),
-    seconds = time.getSeconds()
-    const hoursForClock = hours % 12
+// setTime()
+setInterval(setTime, 1000) // call it every 1s (1000ms) to keep ticking in real-time
 
-    hour.style.transform = `
-    transform: translate(-50%, -100%) rotate(${scale(
-        hoursForClock, 0, 11, 0, 360
-    )}deg)
-    `
+function setTime() {
+    const t = new Date()
+    // console.log(time)
+    const month = t.getMonth(),
+    day = t.getDay(),
+    dte = t.getDate(),
+    hours = t.getHours(),
+    minutes = t.getMinutes(),
+    seconds = t.getSeconds()
+    const hoursForClock = hours % 12,
+    ampm = hours >= 12 ? 'PM' : 'AM'
+
+    hour.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+    minute.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+    second.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+    time.innerHTML = `${hours}:${minutes >= 10 ? minutes : `0${minutes}`}`
+    // todo: hoursForClock can be used for 12-hour clock, using ampm as a suffix
+    date.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${dte}</span>`
 }
 
 function scale(num, in_min, in_max, out_min, out_max) {
