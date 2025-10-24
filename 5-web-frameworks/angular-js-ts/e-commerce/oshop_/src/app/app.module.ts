dfc2343f44ff7138389265e3dfca87fa26366283
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2025-10-24 09:56:54 +0000

    .

--- 5-web-frameworks/angular-js-ts/e-commerce/oshop_/src/app/app.module.ts
+++ 5-web-frameworks/angular-js-ts/e-commerce/oshop_/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -46,10 +47,12 @@ import { LoginComponent } from './login/login.component';
       { path: 'shopping-cart', component: ShoppingCartComponent },
       { path: 'check-out', component: CheckOutComponent },
       { path: 'order-success', component: OrderSuccessComponent },
+      { path: 'my/orders', component: MyOrdersComponent },
       { path: 'login', component: LoginComponent },
       { path: 'admin/products', component: AdminProductsComponent },
       { path: 'admin/orders', component: AdminOrdersComponent },
-    ])
+    ]),
+    NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
