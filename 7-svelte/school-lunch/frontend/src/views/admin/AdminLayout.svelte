deb707b378be88b2acae50db8110dd29f62cd976
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2025-11-08 14:42:23 +0000

    .

---
+++ 7-svelte/school-lunch/frontend/src/views/admin/AdminLayout.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { Route } from 'svelte-router-spa'
+    import { onMount } from 'svelte'
+    import { user } from '../../stores.js'
+    
+    export let currentRoute
+    let initialized = false
+
+    onMount(() => (
+        user.set({
+            name: 'User', schoolName: 'School'
+        }),
+        initialized = true
+    ))
+</script>
+
+<div>
+    Admin Layout
+    {#if initialized}
+        <Route {currentRoute} />
+    {/if}
+</div>
--- 7-svelte/school-lunch/frontend/src/views/admin/LunchMenuAdmin.svelte
+++ 7-svelte/school-lunch/frontend/src/views/admin/LunchMenuAdmin.svelte
@@ -1,7 +1,8 @@
 <script>
+    import { user } from '../../stores.js'
     let name = 'name'
 </script>
 
 <div>
-    <h1 class="title is-4">Lunch Menu Administration / {name}</h1>
+    <h1 class="title is-4">Lunch Menu Administration / {name} / {$user.schoolName}</h1>
 </div>
